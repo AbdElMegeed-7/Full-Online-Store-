7b1a9aa48e7cc954c663138e3ac14513b607f3b9
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-05-02 10:59:08 +0200

    Display Products by Category_Slug

--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from category.models import Category
 from .models import Product
 
 
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    product_counts = products.count()
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(
+            category=categories, is_available=True)
+        product_counts = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_counts = products.count()
 
     context = {
         'products': products,
