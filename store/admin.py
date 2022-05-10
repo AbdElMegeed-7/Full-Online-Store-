199e05241a70ae56bdb32d394fcbba29a813b044
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-05-10 04:24:47 +0200

    Product Variation Model

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -9,3 +9,14 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ['product', 'variation_category',
+                    'variation_value', 'created_date', 'is_active']
+    list_editable = ('is_active',)
+    list_filter = ['product', 'variation_category',
+                   'variation_value', 'is_active']
+
+
+admin.site.register(Variation, VariationAdmin)
