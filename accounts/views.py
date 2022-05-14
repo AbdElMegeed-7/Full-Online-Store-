e9273db3c05ce42d20734d88f04597f33df984a5
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-05-14 22:34:34 +0200

    Registration   View Function

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from .models import Account
 from .forms import RegistrationForm
 
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            phone_number = form.cleaned_data['phone_number']
+            username = first_name + last_name
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password,
+            )
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
 
     context = {'form': form, }
     return render(request, 'accounts/register.html', context)
